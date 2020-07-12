272b629a8efa8fe75cc25bbdc071e4132a67c194
muhammadhyssamhimawan <62740286+muhammadhyssamhimawan@users.noreply.github.com>
2020-07-12 23:47:04 +0700

    Add files via upload

---
+++ denah.php
@@ -0,0 +1,36 @@
+<?php
+
+
+include_once('config.php');
+?>
+
+
+    <section class="content">
+        <div class="container-fluid" style="margin: 120px;">
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                DENAH RUMAH SAKIT
+                            </h2>
+                        </div>
+                        <div class="body">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <a href="javascript:history.go(-1)"><button type="button" class="btn bg-blue btn-block btn-lg waves-effect">KEMBALI KE DEPAN</button></a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+?>
---
+++ pengaduan.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once('config.php');
+
+?>
+
+
+
+    <section class="content">
+        <div class="container-fluid" style="margin: 120px;">
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                PENGADUAN
+                            </h2>
+                        </div>
+                        <div class="body table-responsive">
+
+                        Pengaduan Langsung	: Ruang Unit Pengaduan Masyarakat di dekat pintu masuk lobby Rumah Sakit<br/>
+						SMS/Telepon	: <br/>
+						Kotak Saran	: Kotak saran yang berada di informasi, ruang perawatan inap, dan poliklinik rawat jalan<br/>
+						Email : rsud.humas@bekasikab.go.id<br/>
+       
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <a href="javascript:history.go(-1)"><button type="button" class="btn bg-blue btn-block btn-lg waves-effect">KEMBALI KE DEPAN</button></a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+?>
